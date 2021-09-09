f4436cf717befa421582ba4bfa9b637bcc8603ff
fwornle <fwornle@yahoo.co.uk>
2021-09-09 08:27:41 +0200

    first working version (base for the exercise)

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,11 +17,13 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -30,6 +32,8 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
  * Fragment where the game is played
  */
 class GameFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
 
     // The current word
     private var word = ""
@@ -53,8 +57,9 @@ class GameFragment : Fragment() {
                 false
         )
 
-        // TODO (04) Create and initialize a GameViewModel, using ViewModelProvider; Add a log
-        // statement
+        // establish connection to (possibly already existing) viewModel
+        viewModel =  ViewModelProvider(this).get(GameViewModel::class.java)
+        Log.i("GameFragment", "Established connection to viewModel")
 
         resetList()
         nextWord()
@@ -101,7 +106,7 @@ class GameFragment : Fragment() {
      * Called when the game is finished
      */
     private fun gameFinished() {
-        val action = GameFragmentDirections.actionGameToScore(score)
+        val action = GameFragmentDirections.actionGameToScore()
         findNavController(this).navigate(action)
     }
 
